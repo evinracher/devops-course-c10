4d78293d568bbedae5eaa677f69b832310471b84
evinracher <eevinley@gmail.com>
2022-07-19 14:37:04 -0500

    refactor: delete test script and editing the page message

--- my-infrastructure/test.sh
+++
@@ -1,4 +0,0 @@
-hostname=$(hostname -I)
-echo "<html>" > index.html
-echo "<p>Hello world ${hostname}</p>" >> index.html
-echo "</html>" >> index.html
--- my-infrastructure/user-data.sh
+++ my-infrastructure/user-data.sh
@@ -3,6 +3,6 @@ sudo apt-get update
 sudo apt install apache2 --fix-missing -y
 hostname=$(hostname -I)
 echo "<html>" > index.html
-echo "<p>Hello world ${hostname}</p>" >> index.html
+echo "<p>Hello world from ${hostname}</p>" >> index.html
 echo "</html>" >> index.html
 sudo cp index.html /var/www/html/index.html
